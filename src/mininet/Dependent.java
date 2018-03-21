a6e1e05b6d827761f3bb9c65c840071ec87c4122
s3468489 <s3468489@student.rmit.edu.au>
2018-03-21 22:16:18 +1100

    Modification to Dependent
    Deleted the field *boolean isSixteenYearsOld* and its corresponding getter and setter.

--- src/mininet/Dependent.java
+++ src/mininet/Dependent.java
@@ -11,55 +11,39 @@ public class Dependent extends User
     //who have a single parent
     private Adult parents[];
     /*
-    these two following fields judges whether a particular dependent
-    is less than 3 years-old, or older than 16 years-old,
+    The ollowing field judges whether a particular dependent
+    is less than 3 years-old
     respectively
     */
-    private boolean isThreeYearsOld, isSixteenYearsOld;
+    private boolean isThreeYearsOld;
     
     public Dependent(String name, String userName, int age, Adult[] parents,
-            boolean isThreeYearsOld, boolean isSixteenYearsOld)
+            boolean isThreeYearsOld)
     {
         super(name, userName, age);
         
         this.parents = parents;
         
         this.isThreeYearsOld = isThreeYearsOld;
-        
-        this.isSixteenYearsOld = isSixteenYearsOld;
-        
     }
-        
-        
-        public Adult[] getParents()
-        {
-            return parents;
-        }
-        
-        public void setParents(Adult[] parents)
-        {
-            this.parents = parents;
-        }
-        
-        public boolean isThreeYearsOld()
-        {
-            return isThreeYearsOld;
-        }
-        
-        public void setThreeYearsOld(boolean isThreeYearsOld)
-        {
-            this.isThreeYearsOld = isThreeYearsOld;
-        }
-        
-        public boolean isSixteenYearsOld()
-        {
-            return isSixteenYearsOld;
-        }
-        
-        public void setSixteenYearsOld(boolean isSixteenYearsOld)
-        {
-            this.isSixteenYearsOld = isSixteenYearsOld;
-        }
-        
+      
+    public Adult[] getParents()
+    {
+        return parents;
+    }
+    
+    public void setParents(Adult[] parents)
+    {
+        this.parents = parents;
+    }
+    
+    public boolean isThreeYearsOld()
+    {
+        return isThreeYearsOld;
+    }
+    
+    public void setThreeYearsOld(boolean isThreeYearsOld)
+    {
+        this.isThreeYearsOld = isThreeYearsOld;
+    }   
 }
-
